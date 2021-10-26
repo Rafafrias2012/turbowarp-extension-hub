12cfc628048061d1c29d104bd9f2b474f0b7f0c3
lopste <80974914+lopste@users.noreply.github.com>
2021-10-25 18:34:43 -0700

    added more blocks

--- extension-files/helpful-blocks.js
+++ extension-files/helpful-blocks.js
@@ -8,12 +8,30 @@ class helpfulBlocks {
           opcode: 'isalwaystrue',
           blocktype: Scratch.BlockType.BOOLEAN,
           text: 'true'
+        },
+        {
+          opcode: 'isalwaysfalse',
+          blocktype: Scratch.BlockType.BOOLEAN,
+          text: 'false'
+        },
+        {
+          opcode: 'xorgate',
+          blocktype: Scratch.BlockType.BOOLEAN,
+          text: '[ONE] xor [TWO]',
+          arguments: {
+            ONE: {
+              type: Scratch.ArgumentType.BOOLEAN
+            },
+            TWO: {
+              type: Scratch.ArgumentType.BOOLEAN
+            }
+          }
         }
       ]
     }
   };
-  isalwaystrue() {
-    return true
-  };
+  isalwaystrue(){return true};
+  isalwaysfalse(){return false};
+  xorgate(args) {return (((args.ONE || args.TWO) && (!(args.ONE && args.TWO))))
 }
 Scratch.extensions.register(new helpfulBlocks());
